8db839181fdf8d4035d82f2ef22ec0b6cde7e96d
Alexey Nesterenko <alexei.ne@gmail.com>
2026-05-18 15:28:44 +0700

    refactoring

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Recorder from '~/components/Recorder.svelte';
-  import Sandbox from './components/Sandbox.svelte';
+  import Sandbox from '~/components/Sandbox.svelte';
 </script>
 
 <main class="min-w-200 bg-amber-50 p-10">
