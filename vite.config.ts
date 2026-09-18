bcac0ec0d7e37766734dff8c3b3fe86ab2a34f96
Alexey Nesterenko <alexei.ne@gmail.com>
2026-09-18 13:39:41 +0700

    prepare for using github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), svelte()],
   resolve: {
     alias: {
-      "~": resolve(__dirname, "src"),
+      '~': resolve(__dirname, 'src'),
     },
   },
+  base: '/voice-mirror/',
 });
